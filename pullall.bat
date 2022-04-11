8728dd4a31cada2a310be7bfabb829399edc469b
alfie <alfienurse@gmail.com>
2022-04-11 18:39:14 +0100

    stuff

--- pullall.bat
+++ pullall.bat
@@ -1,1 +1,2 @@
-git pull origin main
+:: Step 1: Pull (ie copy) any changes from github to local repository.
+git pull github main
--- pushall.bat
+++ pushall.bat
@@ -1,4 +1,10 @@
+:: Step 1: Add all changed files to your local git repository.
+::         This marks all the files which are changed in the local repository.
 git add --all
+:: Step 2: Show all changes to your local git.
 git status
+:: Step 3: Commit all changes to your local git - This saves the changes in the local repository.
+::         Use -m to add a commit message "stuff" - this can be anything.
 git commit -m "stuff"
-git push origin main
+:: Step 4: Push (ie copy) all the changes to your local git to GitHub.
+git push github main
