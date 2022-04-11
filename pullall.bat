63b192a1aee1e5f65e1fc1cadc993af25ba8324f
martin <mjnurse@gmail.com>
2022-04-11 18:56:42 +0100

    changes made by martin

--- pullall.bat
+++ pullall.bat
@@ -1,3 +1,4 @@
 git stash
+
 :: Step 2: Pull (ie copy) any changes from github to local repository.
 git pull github main
--- pushall.bat
+++ pushall.bat
@@ -1,10 +1,13 @@
 :: Step 1: Add all changed files to your local git repository.
 ::         This marks all the files which are changed in the local repository.
 git add --all
+
 :: Step 2: Show all changes to your local git.
 git status
+
 :: Step 3: Commit all changes to your local git - This saves the changes in the local repository.
 ::         Use -m to add a commit message "change made by alfie" - this can be anything.
 git commit -m "changes made by alfie"
+
 :: Step 4: Push (ie copy) all the changes to your local git to GitHub.
 git push github main
